3f45839b23ea868135dbf9321588829fbee5c178
Gaurav Borse <gborse8055@gmail.com>
2026-02-10 11:57:24 +0530

    Remove the push and pull functionality temporary

--- controllers/pull.js
+++ controllers/pull.js
@@ -1,41 +1,4 @@
-import fs from "fs/promises";
-import path from "path";
-import { s3, S3_BUCKET } from "../config/aws-config.js";
-
-export async function pullRepo() {
-    try {
-        const repoPath = path.resolve(process.cwd(), ".apnaGit");
-        const commitsPath = path.join(repoPath, "commits");
-
-        const data = await s3.listObjectsV2({
-            Bucket: S3_BUCKET,
-            Prefix: "commits/",
-        }).promise();
-
-        const objects = data.Contents || [];
-
-        for (const object of objects) {
-            const key = object.Key; // commits/<commitId>/file.txt
-
-            const commitId = key.split("/")[1];
-            const commitDir = path.join(commitsPath, commitId);
-
-            await fs.mkdir(commitDir, { recursive: true });
-
-            const file = await s3.getObject({
-                Bucket: S3_BUCKET,
-                Key: key,
-            }).promise();
-
-            const fileName = path.basename(key);
-            await fs.writeFile(
-                path.join(commitDir, fileName),
-                file.Body
-            );
-        }
-
-        console.log("All commits pulled from S3.");
-    } catch (err) {
-        console.error("Unable to pull:", err);
-    }
+export async function pushRepo(){
+    console.log("pushed the repo");
+    console.log("Feature is unavilable")
 }
